a2d67091cee31db7d72927cf6dcc4e0db33d4db5
shubham-1809 <shubhamchouksey1809@gmail.com>
2023-12-15 18:18:28 +0530

    added files

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -141,7 +141,7 @@ function SideDrawer() {
             </Text>
           </Button>
         </Tooltip>
-        <Text fontSize="2xl" fontFamily="Work sans">
+        <Text fontSize="2xl" fontFamily="Work sans" fontWeight="bold">
           CHAT SPHERE
         </Text>
         <div>
